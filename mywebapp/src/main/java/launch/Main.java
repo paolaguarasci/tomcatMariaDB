f64d669547c9304a4303dabc1c67a729ed0d87fb
Paola Guarasci <paolaguarasci@gmail.com>
2020-12-11 16:30:56 +0100

    app funzionante

--- mywebapp/src/main/java/launch/Main.java
+++ mywebapp/src/main/java/launch/Main.java
@@ -1,6 +1,7 @@
+package launch;
+
 import java.io.File;
 
-import org.apache.catalina.LifecycleException;
 import org.apache.catalina.WebResourceRoot;
 import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
@@ -13,40 +14,28 @@ public class Main {
 
         String webappDirLocation = "src/main/webapp/";
         Tomcat tomcat = new Tomcat();
-        String webPort = System.getenv("PORT");
 
+        //The port that we should run on can be set into an environment variable
+        //Look for that variable and default to 8080 if it isn't there.
+        String webPort = System.getenv("PORT");
         if(webPort == null || webPort.isEmpty()) {
-          webPort = "8080";
+            webPort = "8000";
         }
 
         tomcat.setPort(Integer.valueOf(webPort));
-        StandardContext ctx = (StandardContext) tomcat.addWebapp("", new
-        File(webappDirLocation).getAbsolutePath());
 
-        System.out.println("configuring app with basedir: " + new File("./" +
-        webappDirLocation).getAbsolutePath());
+        StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
 
+        // Declare an alternative location for your "WEB-INF/classes" dir
+        // Servlet 3.0 annotation will work
         File additionWebInfClasses = new File("target/classes");
-
         WebResourceRoot resources = new StandardRoot(ctx);
-
         resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
-
-        additionWebInfClasses.getAbsolutePath(), "/"));
-
+                additionWebInfClasses.getAbsolutePath(), "/"));
         ctx.setResources(resources);
 
-        tomcat.enableNaming();
-
-        tomcat.getConnector();
-
-        try {
-          tomcat.start();
-        } catch (LifecycleException e) {
-          e.printStackTrace();
-        }
+        tomcat.start();
         tomcat.getServer().await();
-
     }
-
 }
